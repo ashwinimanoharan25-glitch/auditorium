e741866e1432f117d39c34805cab015756e403c5
Ashwini <ashwinimanoharan25@gmail.com>
2026-04-27 12:36:06 +0530

    Add booking functionality with models, views, and templates

--- auditorium/project/urls.py
+++ auditorium/project/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('history/',views.history,name='history'),
+    path('booking/', views.booking_page, name='booking'),
 ]
--- auditorium/project/views.py
+++ auditorium/project/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from .models import booking
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html')
 
+def history(request):
+    bookings = booking.objects.all()
+    return render(request, 'history.html', {'bookings': bookings})
+
+
+from django.contrib import messages
+
+def booking_page(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        date = request.POST.get('date')
+        time = request.POST.get('time')
+
+        booking.objects.create(
+            name=name,
+            date=date,
+            time=time
+        )
+
+        messages.success(request, "Booking Successful!")  # ✅ first
+        return redirect('history')  # ✅ then redirect
+
+    return render(request, 'booking.html')
