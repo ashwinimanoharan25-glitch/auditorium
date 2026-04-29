1098cf3ba6668e8cc4db47aeae68f0fdd7dbd252
Ashwini <ashwinimanoharan25@gmail.com>
2026-04-29 10:13:19 +0530

    chnages added in settings.p

--- auditorium/auditorium/wsgi.py
+++ auditorium/auditorium/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auditorium.auditorium.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auditorium.settings')
 
 application = get_wsgi_application()
--- auditorium/manage.py
+++ auditorium/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auditorium.auditorium.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auditorium.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
