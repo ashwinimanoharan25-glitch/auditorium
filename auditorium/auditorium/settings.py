71d6541d9d891ecbddb95c822b40b37832e222b5
Ashwini <ashwinimanoharan25@gmail.com>
2026-04-29 09:17:44 +0530

    update

--- auditorium/auditorium/settings.py
+++ auditorium/auditorium/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v-)-5c#zemoevk8k95=$@cg=(d!4v268hs3xoobkahjg+8gdyo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'auditorium.urls'
